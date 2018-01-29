a6818415ae48da42e2b15592c476e811ef547ed9
alexzlatanov <alex.zlatanov89@abv.bg>
2018-01-29 11:52:55 +0200

    My App

---
+++ js/remote.js
@@ -0,0 +1,46 @@
+let remote = (() => {
+    const baseUrl = 'https://baas.kinvey.com/';
+    const appKey = 'kid_BJiMSvAt-';
+    const appSecret = 'c2dfa87c4d7e494699aa88013a538f01';
+
+    function makeAuth(type) {
+        if (type === 'basic') return 'Basic ' + btoa(appKey + ':' + appSecret);
+        else return 'Kinvey ' + localStorage.getItem('authtoken');
+    }
+
+    function makeRequest(method, module, url, auth) {
+        return req = {
+            url: baseUrl + module + '/' + appKey + '/' + url,
+            method,
+            headers: {
+                'Authorization': makeAuth(auth)
+            }
+        };
+    }
+
+    function get(module, url, auth) {
+        return $.ajax(makeRequest('GET', module, url, auth));
+    }
+
+    function post(module, url, data, auth) {
+        let req = makeRequest('POST', module, url, auth);
+        req.data = JSON.stringify(data);
+        req.headers['Content-Type'] = 'application/json';
+        return $.ajax(req);
+    }
+
+    function update(module, url, data, auth) {
+        let req = makeRequest('PUT', module, url, auth);
+        req.data = JSON.stringify(data);
+        req.headers['Content-Type'] = 'application/json';
+        return $.ajax(req);
+    }
+
+    function remove(module, url, auth) {
+        return $.ajax(makeRequest('DELETE', module, url, auth));
+    }
+
+    return {
+        get, post, update, remove
+    }
+})();
